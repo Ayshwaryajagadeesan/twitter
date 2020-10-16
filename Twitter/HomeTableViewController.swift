576358a26ceb46ec637249794c36bde296ca86d9
Ayshwarya Jagadeesan <jayshwarya@gmail.com>
2020-10-15 21:45:08 -0400

    Twitter Part2

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,8 +18,15 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
        
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.loadTweets()
+    }
+    
     @objc func loadTweets(){
         
         numberofTweets = 20
@@ -36,6 +43,7 @@ class HomeTableViewController: UITableViewController {
             print("could not retrive tweet! oh no!")
         })
     }
+    
     
     func loadMoreTweets(){
          let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -87,8 +95,11 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         return cell
+        cell.setRetweet(tweetArray[indexPath.row]["retweeted"] as! Bool)
     }
     
     
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -17,10 +17,81 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var userNameLabel: UILabel!
     
+    @IBOutlet weak var retwetButton: UIButton!
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var favButton: UIButton!
+    var favorited:Bool = false
+    var tweetId: Int = -1
+    var retweeted:Bool = false
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if(toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("favorite did not sucseed:\(Error)")
+            })
+        }
+        else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                print("unfavorite did not succeed:\(Error)")
+            })
+        }
+    }
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+        let toBeRetweeted = !retweeted
+         if(toBeRetweeted){
+             TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+                 self.setRetweet(true)
+             }, failure: { (Error) in
+                 print("retweet did not sucseed:\(Error)")
+             })
+         }
+         else{
+             TwitterAPICaller.client?.unretweet(tweetId: tweetId, success: {
+                 self.setRetweet(false)
+             }, failure: { (Error) in
+                 print("unretweet did not succeed:\(Error)")
+             })
+         }
+    }
 
+    func setFavorite(_ isfavorited:Bool){
+        favorited = isfavorited
+        if(favorited){
+        favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else
+        {
+favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+        
+    }
+    func setRetweet(_ isretweeted:Bool){
+       retweeted = isretweeted
+        if(retweeted){
+            retwetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+           // retwetButton.isEnabled = false
+            
+        }
+        else{
+         retwetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+           // retwetButton.isEnabled = true
+            
+        }
+    }
+    
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
